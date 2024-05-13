58938e6df7053048a371d9e671bba63aef2d2fda
MikolajR13 <piotrek.trebeusz@gmail.com>
2024-05-14 01:52:18 +0200

    dodane updatowanie wraz z dodawaniem do bazy danych :)

--- Model/WeatherHourly.cs
+++ Model/WeatherHourly.cs
@@ -2,24 +2,24 @@
 {
     public class WeatherHourly //wszystko z API oprócz podpisanych 
     {
-        public int Id { get; set; }
-        public long Dt { get; set; }
+        public int Id { get; set; } //
+        public long Dt { get; set; } //
         public int WeatherDailyId { get; set; } //klucz obcy do relacji z WeatherDaily - ustawiany ręcznie w ReadDataBaseService.cs (katalog Services)
         public DateTime DateHourly { get; set; } //Data ustawiana ręcznie w Weather.razor (24-12-2024 00:00:00)
         public TimeSpan Time { get; set; } //czas ustawiany ręcznie w Weather.razor (12:32:11)
-        public double Temp { get; set; }
-        public double Feels_like { get; set; }
-        public int Pressure { get; set; }
-        public int Humidity { get; set; }
-        public double Dew_point { get; set; }
-        public double Uvi { get; set; }
-        public int Clouds { get; set; }
+        public double Temp { get; set; } //
+        public double Feels_like { get; set; } //
+        public int Pressure { get; set; } //
+        public int Humidity { get; set; } //
+        public double Dew_point { get; set; } //
+        public double Uvi { get; set; } //
+        public int Clouds { get; set; } //
         public int Visibility { get; set; }
-        public double Wind_speed { get; set; }
-        public double Wind_gust { get; set; }
-        public int Wind_deg { get; set; }
-        public List<Weather> Weather { get; set; } //zmienna nawigacyjna do Weather.cs
-        public double Pop { get; set; }
+        public double Wind_speed { get; set; } //
+        public double Wind_gust { get; set; } //
+        public int Wind_deg { get; set; } //
+        public List<Weather> Weather { get; set; } //zmienna nawigacyjna do Weather.cs //
+        public double Pop { get; set; } //
         public WeatherDaily WeatherDaily { get; set; }   //zmienna nawigacyjna do WeatherDaily.cs
     }
 }
--- Services/WeatherService.cs
+++ Services/WeatherService.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Instrukcja.Data;
 using Instrukcja.Model;
+using Instrukcja.Components.Pages;
 
 namespace Instrukcja.Services //wszystkie metody służące do zapisu, updatu i usuwania danych z bazy dancyh
 {
@@ -21,15 +22,58 @@ namespace Instrukcja.Services //wszystkie metody służące do zapisu, updatu i 
             return await _context.WeatherData.ToListAsync();
         }
 
-        public async Task AddWeatherDailyAsync(WeatherDaily weather)
+        //ta funkcja działa tak, że wyszukuje w bazie danych rekord, który ma takie same pole DateDaily  jak rekord, który chcemy dodać do bazy danych
+        //jeżeli znajdzie taki rekord to zamiast dodawać rekord to aktualizuje obecny. Jeżeli nie znajdzie to dodaje nowy rekord weather
+        public async Task UpdateOrAddWeatherDailyAsync(WeatherDaily weather)
         {
-            _context.WeatherData.Add(weather);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task UpdateWeatherDailyAsync(WeatherDaily weather)
-        {
-            _context.WeatherData.Update(weather);
+            var existingWeather = await _context.WeatherData
+                .Include(w => w.Temp) //uwzględnianie Temp żeby ręcznie je zaktualizować, wpp będą nam się rekordy duplikowały
+                .Include(w => w.Feels_like) //to samo co u góry
+                .Include(w => w.Weather) // to samo co u góry
+                .FirstOrDefaultAsync(w => w.DateDaily == weather.DateDaily);
+            if (existingWeather != null) // jeżeli nie jest pusty to 
+            {
+                _context.Entry(existingWeather).CurrentValues.SetValues(weather); //wszystkie normalne pola updatujemy ( czyli int string itp itd)
+                if(existingWeather.Temp !=null) //sprawdzamy czy jest Temp jakieś i jezeli jest to
+                {
+                    _context.Entry(existingWeather.Temp).CurrentValues.SetValues(weather.Temp); //ręcznie robimy update
+                }
+                else // jeżeli nie to dodajemy nowe Temp
+                {
+                    existingWeather.Temp = weather.Temp;
+                }
+                if(existingWeather.Feels_like != null) //to samo co dla Temp
+                {
+                    _context.Entry(existingWeather.Feels_like).CurrentValues.SetValues(weather.Feels_like);
+                }
+                else
+                {
+                    existingWeather.Feels_like = weather.Feels_like;
+                }
+                if(existingWeather.Weather != null) //To samo co dla Temp
+                {
+                    _context.Entry(existingWeather.Weather).CurrentValues.SetValues(weather.Weather);
+                }
+                else
+                {
+                    existingWeather.Weather = weather.Weather;
+                }
+                if(existingWeather.WeatherHourlies != null) //To samo co dla Temp
+                {
+                    _context.Entry(existingWeather.WeatherHourlies).CurrentValues.SetValues(weather.WeatherHourlies);
+                }
+                else
+                {
+                    existingWeather.WeatherHourlies = weather.WeatherHourlies;
+                }
+                           
+                _context.WeatherData.Update(existingWeather); //update
+            }
+            else
+            {
+                _context.WeatherData.Add(weather); //lub dodanie jeżeli nie znaleźliśmy rekordu
+            }
+            
             await _context.SaveChangesAsync();
         }
 
@@ -40,32 +84,50 @@ namespace Instrukcja.Services //wszystkie metody służące do zapisu, updatu i 
         }
 
         // Metody dla WeatherHourly
-        public async Task<List<WeatherHourly>> GetAllWeatherHourliesAsync()
+        public async Task<List<Model.WeatherHourly>> GetAllWeatherHourliesAsync()
         {
             return await _context.WeatherHourlyData.ToListAsync();
         }
 
-        public async Task AddWeatherHourlyAsync(WeatherHourly weatherHourly)
+        //ta funkcja działa tak, że wyszukuje w bazie danych rekord, który ma takie same pola DateHourly oraz Time jak rekord, który chcemy dodać do bazy danych
+        //jeżeli znajdzie taki rekord to zamiast dodawać rekord to aktualizuje obecny. Jeżeli nie znajdzie to dodaje nowy rekord weatherHourly
+        public async Task UpdateOrAddWeatherHourlyAsync(Model.WeatherHourly weatherHourly) 
         {
-            // WAŻNE - nadawanie klucza obcego w obiekcie weatherHourly - szukamy dnia, który ma taki sam dzień i przypisujemy obiektowi weatherHourly klucz pomocniczy weatherHourly.WeatherDailyId = WeatherDaily.Id
-            var matchDaily = await _context.WeatherData
-                .FirstOrDefaultAsync(wd => wd.DateDaily.Date == weatherHourly.DateHourly.Date);
-            if (matchDaily != null)
-            {
-                weatherHourly.WeatherDailyId = matchDaily.Id;
-            }
-            else
-            {
-                throw new Exception("No matching WeatherDaily record found for the given date!");
-            }
-            _context.WeatherHourlyData.Add(weatherHourly);
-            await _context.SaveChangesAsync();
-        }
+            //wyszukiwanie czy jest rekord o takiej samej DateHourly oraz Time
+            var existingWeather = await _context.WeatherHourlyData
+                .Include(w => w.Weather) //uwzględniamy Weather bo jeżeli tego nie zrobimy to będą nam się duplikowały rekordy
+                .FirstOrDefaultAsync(w => w.DateHourly == weatherHourly.DateHourly && w.Time == weatherHourly.Time);
+            if (existingWeather != null) { //jeżeli istnieje już taki rekord
 
-        public async Task UpdateWeatherHourlyAsync(WeatherHourly weatherHourly)
-        {
-            _context.WeatherHourlyData.Update(weatherHourly);
-            await _context.SaveChangesAsync();
+                _context.Entry(existingWeather).CurrentValues.SetValues(weatherHourly); //update wszystkich normalnych pól ( typu int, string, date time itp itd)
+                if(existingWeather.Weather != null) //jeżeli Weather nie jest puste 
+                {
+                    _context.Entry(existingWeather.Weather).CurrentValues.SetValues(weatherHourly); //update Weather
+                }
+                else //jeżeli jest puste to dodajemy
+                {
+                    existingWeather.Weather = weatherHourly.Weather;
+                }
+
+                _context.WeatherHourlyData.Update(existingWeather); //update
+            }
+            else //jeżeli nie ma jeszcze takiego rekordu co ma taką samą datę i godzinę to 
+            {   // WAŻNE - nadawanie klucza obcego w obiekcie weatherHourly - szukamy dnia, który ma taki sam dzień i przypisujemy obiektowi weatherHourly klucz pomocniczy weatherHourly.WeatherDailyId = WeatherDaily.Id 
+                var matchDaily = await _context.WeatherData
+                .FirstOrDefaultAsync(wd => wd.DateDaily.Date == weatherHourly.DateHourly.Date);
+
+                    if (matchDaily != null)
+                    {
+                        weatherHourly.WeatherDailyId = matchDaily.Id;
+                    }
+                    else
+                    {
+                        throw new Exception("No matching WeatherDaily record found for the given date!");
+                    }
+                    _context.WeatherHourlyData.Add(weatherHourly); //dodanie nowego rekordu
+            }
+             
+            await _context.SaveChangesAsync();  //save
         }
 
         public async Task DeleteWeatherHourlyAsync(WeatherHourly weatherHourly)
